ae20448598a2f6f4f56fbb748ec0fce3f1edf262
celeloup <celia.m.leloup@gmail.com>
2022-11-25 16:54:24 +0100

    added nyffin mascot

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import DecoDots from './Svg/DecoDots';
+import NyffinMascot from './Svg/NyffinMascot';
 
 const Landing = () => {
 	return (
-		<div className="bg-nyffinRed h-[300px] z-[20]s">
-			<p className="text-center text-[200px] text-white">THE FUTURE</p>
+		<div className="bg-nyffinRed z-[20]s pt-[50px]">
+			<div className="pt-[50px]">
+				<p className=" inline-block pb-[10px] font-bebasNeue text-[100px] text-white text-outline tracking-[15px] leading-[60px]">
+					THE FUTURE OF
+				</p>
+				<p className="font-anton text-[100px] text-white tracking-[5px]">
+					VIRTUAL REALITY
+				</p>
+			</div>
+			<NyffinMascot className="block" />
+			<DecoDots />
 		</div>
 	);
 };
