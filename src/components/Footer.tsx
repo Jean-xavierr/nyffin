2e498f6563f4cbca3fcbf9ae3ecbfccc7387e02e
celeloup <celia.m.leloup@gmail.com>
2022-12-01 13:40:14 +0100

    Footer is responsive

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,38 +1,42 @@
 import React from 'react';
 import SocialBar from '~/src/components/SocialBar';
+import Email from './Svg/Email';
 import NyffinLogo from './Svg/NyffinLogo';
 
 const Footer = () => {
 	return (
 		<React.Fragment>
-			<div className="w-full bg-nyffinBlack text-white">
+			<div className="w-full bg-nyffinBlack text-white p-[10px]">
 				<div className="max-w-[950px] m-auto px-5">
-					<div className="py-10">
-						<div className="grid grid-cols-2 items-center gap-y-3">
-							<NyffinLogo className="w-[100px] h-[40px]" />
+					<div className="pt-10 sm:pb-[70px]">
+						<div className="grid grid-cols-1 sm:grid-cols-2 items-center justify-items-center sm:justify-items-start gap-y-3">
+							<NyffinLogo className="w-[120px] h-[45px]" />
+
 							<SocialBar
-								className="w-[300px] justify-self-end"
-								iconClassName="h-[22px] w-[22px]"
+								className="w-full max-w-[320px] sm:w-[320px] mb-[18px] sm:mb-0 sm:justify-self-end order-4 sm:order-2"
+								iconClassName="h-[25px] w-[25px]"
 							/>
-							<p>nyffin.esport@gmail.com</p>
-							<div className="justify-self-end font-alumniSans text-2xl w-[300px] flex justify-between">
+
+							<Email className="order-3 my-[20px] sm:my-0" />
+
+							<div className="sm:justify-self-end font-alumniSans text-[30px] tracking-[2px] w-full max-w-[320px] sm:max-w-[380px] sm:min-w-[320px] flex justify-between flex-col sm:flex-row order-2 sm:order-4 my-[20px] sm:my-0">
 								<a href="#about" className=" hover:underline">
 									About
 								</a>
-								<span className="w-[1px] h-[22px] bg-white mt-1"></span>
+								<span className="sm:w-[1px] sm:h-[28px] bg-white mt-2"></span>
 								<a href="#teams" className=" hover:underline">
 									Teams
 								</a>
-								<span className="w-[1px] h-[22px] bg-white mt-1"></span>
+								<span className="sm:w-[1px] sm:h-[28px] bg-white mt-2"></span>
 								<a href="#achievements" className=" hover:underline">
 									Achievements
 								</a>
 							</div>
 						</div>
 					</div>
-					<div className="flex justify-between font-alumniSans border-t py-2 tracking-[2px]">
+					<div className="flex flex-col sm:flex-row justify-between font-alumniSans border-t py-3 tracking-[1px] text-[17px]">
 						<p className=" flex-grow">2022, FR - Nyffin Esports.</p>
-						<p className=" pr-20">Desgin by @kooks</p>
+						<p className=" pr-20">Design by @kooks</p>
 						<p>Developped by @kooks & @maarc</p>
 					</div>
 				</div>
